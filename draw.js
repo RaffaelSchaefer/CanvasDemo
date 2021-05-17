17940e339ae411b979ce3a664a64eafb22ea9c97
Raffael Schäfer <raffael.elias.schaefer@gmail.com>
2021-05-17 09:24:45 +0200

    Canvas uses Screen DPI

--- draw.js
+++ draw.js
@@ -1,11 +1,11 @@
 import {colorNoise, bwNoise} from './noise.js';
+import {setupHighDpiCanvas} from "./setup.js";
 import {bwGradient, colorGradient} from './gradient.js';
 //Screen size calculation
 let screenHeight = window.innerHeight;
 let screenWidth = window.innerWidth;
 //Canvas implementation
-let canvas = document.getElementById("draw_zone");
-let canvasContext = canvas.getContext("2d");
+let canvasContext = setupHighDpiCanvas(document.getElementById("draw_zone"));
 //Draw
 canvasContext.imageSmoothingEnabled = false;
 colorNoise(canvasContext, screenWidth, screenHeight);
