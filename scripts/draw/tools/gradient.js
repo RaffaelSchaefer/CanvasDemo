3df3c961289ab8318a3f99463433f87a10381e62
Raffael Schaefer <69517972+RaffaelSchaefer@users.noreply.github.com>
2021-05-17 17:41:21 +0200

    Small change

--- scripts/draw/tools/gradient.js
+++ scripts/draw/tools/gradient.js
@@ -12,8 +12,8 @@ export function rgbGradient (ctx, width, height, resolution) {
     for (let x = 0; x <= width/resolution; x++) {
         for (let y = 0; y <= height/resolution; y++) {
             let rValue = x*resolution%256;
-            let gValue = y*resolution%256;
-            let bValue = 0;
+            let bValue = y*resolution%256;
+            let gValue = 0;
             ctx.fillStyle = "rgb("+rValue+","+gValue+","+bValue+")";
             ctx.fillRect(x*resolution,y*resolution,resolution,resolution);
         }
