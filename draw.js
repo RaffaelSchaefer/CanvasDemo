64b9647a1179f23beae446e123b071023569a5de
Raffael Schäfer <raffael.elias.schaefer@gmail.com>
2021-05-17 11:24:35 +0200

    Working on toolbar

--- draw.js
+++ draw.js
@@ -1,10 +1,15 @@
 import {colorNoise, bwNoise} from './noise.js';
 import {setupHighDpiCanvas} from "./setup.js";
 import {bwGradient} from './gradient.js';
+
 //Screen size calculation
 let screenHeight = window.innerHeight;
 let screenWidth = window.innerWidth;
 //Canvas implementation
 let canvasContext = setupHighDpiCanvas(document.getElementById("draw_zone"));
 //Draw
-colorNoise(canvasContext, screenWidth, screenHeight, 16);
+colorNoise(canvasContext, screenWidth, screenHeight, 4);
+//Event Listeners
+document.getElementById("bwNoise").addEventListener('click',bwNoise(canvasContext, screenWidth, screenHeight, 4));
+document.getElementById("bwGradient").addEventListener('click',bwGradient(canvasContext, screenWidth,screenHeight, 4));
+document.getElementById("coloredNoise").addEventListener('click',colorNoise(canvasContext,screenWidth,screenHeight,4));
