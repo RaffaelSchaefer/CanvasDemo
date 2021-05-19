7a2f7f53e5764b81a6f03de75ad479c71790a060
Raffael Schaefer <69517972+RaffaelSchaefer@users.noreply.github.com>
2021-05-19 10:51:46 +0200

    Window now minimizable

--- scripts/ui/ui.js
+++ scripts/ui/ui.js
@@ -1,3 +1,5 @@
 import {dragUiElement} from "./window/drag.js";
+import {visibilityHandler} from "./window/close.js";
 
 dragUiElement(document.getElementById("toolBox"));
+document.getElementById('toolBoxHeaderClose').addEventListener("click", function () {visibilityHandler(document.getElementById('toolBoxContent'))});
