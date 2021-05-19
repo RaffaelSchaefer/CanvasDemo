d38542c81591f714c84725698949ee588019a194
Raffael Schaefer <69517972+RaffaelSchaefer@users.noreply.github.com>
2021-05-19 14:32:11 +0200

    New Menu option

--- scripts/ui/ui.js
+++ scripts/ui/ui.js
@@ -2,4 +2,6 @@ import {dragUiElement} from "./window/drag.js";
 import {visibilityHandler} from "./window/close.js";
 
 dragUiElement(document.getElementById("toolBox"));
+dragUiElement(document.getElementById("optionsMenu"));
 document.getElementById('toolBoxHeaderClose').addEventListener("click", function () {visibilityHandler(document.getElementById('toolBoxContent'))});
+document.getElementById('optionsMenuClose').addEventListener("click", function () {visibilityHandler(document.getElementById('optionsMenuContent'))});
