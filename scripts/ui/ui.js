b36259bd8dc9ea392751193f7b47fb8d948562db
Raffael Schäfer <raffael.elias.schaefer@gmail.com>
2021-05-21 17:21:49 +0200

    Added footer and trimmed the menus

--- scripts/ui/ui.js
+++ scripts/ui/ui.js
@@ -6,11 +6,9 @@ previewColor(document.getElementById('colorPreview'),document.getElementById('co
 
 dragUiElement(document.getElementById("toolBox"));
 dragUiElement(document.getElementById("optionsMenu"));
-dragUiElement(document.getElementById("brushMenu"));
 dragUiElement(document.getElementById("colorMenu"));
 document.getElementById('toolBoxHeaderClose').addEventListener("click", function () {visibilityHandler(document.getElementById('toolBoxContent'))});
 document.getElementById('optionsMenuClose').addEventListener("click", function () {visibilityHandler(document.getElementById('optionsMenuContent'))});
-document.getElementById('brushMenuClose').addEventListener("click", function () {visibilityHandler(document.getElementById('brushMenuContent'))});
 document.getElementById('colorMenuClose').addEventListener("click", function () {visibilityHandler(document.getElementById('colorMenuContent'))});
 
 document.getElementById('colorRValue').addEventListener("click",function () {previewColor(document.getElementById('colorPreview'),document.getElementById('colorRValue').value,document.getElementById('colorGValue').value,document.getElementById('colorBValue').value)});
