23519b0bd94a685bc6e2e36cc622695c744181f7
Raffael Schäfer <raffael.elias.schaefer@gmail.com>
2021-05-27 09:43:48 +0200

    Added new classes (colorRGB & colorRGBA)

---
+++ scripts/draw/util/color.ts
@@ -0,0 +1,35 @@
+export class colorRGB {
+    r: number;
+    g: number;
+    b: number;
+    constructor(r: number, g: number, b: number) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+    colorGenerate (r: number, g: number, b: number) {
+        return "rgb("+r+", "+g+", "+b+")";
+    }
+    get color() {
+        return this.colorGenerate(this.r, this.g, this.b);
+    }
+}
+
+export class colorRGBA {
+    r: number;
+    g: number;
+    b: number;
+    a: number;
+    constructor(r: number, g: number, b: number, a: number) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+        this.a = a
+    }
+    colorGenerate (r: number, g: number, b: number, a: number) {
+        return "rgb("+r+", "+g+", "+b+", "+a+")";
+    }
+    get color() {
+        return this.colorGenerate(this.r, this.g, this.b, this.a);
+    }
+}
