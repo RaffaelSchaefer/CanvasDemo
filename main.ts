4dad7bfedc0fa0dbd1c82f626f754f291df7e2be
Raffael Schäfer <raffael.elias.schaefer@gmail.com>
2021-05-28 20:50:37 +0200

    Changes in the file structure

--- main.ts
+++ main.ts
@@ -11,4 +11,12 @@ function createWindow () {
 
 app.whenReady().then(() => {
     createWindow()
+
+    app.on('activate', function () {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+app.on('window-all-closed', function () {
+    if (process.platform !== 'darwin') app.quit()
 })
