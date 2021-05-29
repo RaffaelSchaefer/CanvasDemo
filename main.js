0e240cf3c244753078670e163b44483f66c7b1e0
Raffael Schäfer <raffael.elias.schaefer@gmail.com>
2021-05-29 18:11:40 +0200

    Working on the main menu

--- main.js
+++ main.js
@@ -1,19 +1,48 @@
 "use strict";
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-    icon: __dirname + "img/app_icon/app_icon.iconset/icon_512@2x.png",
+    webPreferences: {
+      nodeIntegration: true,
+    },
+    icon: __dirname + "img/app_icon/app_icon.icns",
   });
   win.loadFile("index.html");
 }
 app.whenReady().then(() => {
   createWindow();
   app.on("activate", function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
   });
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  Menu.setApplicationMenu(mainMenu);
 });
 app.on("window-all-closed", function () {
-  if (process.platform !== "darwin") app.quit();
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
+//Menu template
+const mainMenuTemplate = [
+  {
+    label: "File",
+    subMenu: [
+      {
+        label: "Export",
+      },
+      {
+        label: "Quit",
+        click() {
+          app.quit;
+        },
+      },
+    ],
+  },
+  {
+    label: "Tools",
+  },
+];
--- main.ts
+++ main.ts
@@ -1,10 +1,13 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, Menu } = require("electron");
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 800,
     height: 600,
-    icon: __dirname + "img/app_icon/app_icon.iconset/icon_512@2x.png",
+    webPreferences: {
+      nodeIntegration: true,
+    },
+    icon: __dirname + "img/app_icon/app_icon.icns",
   });
 
   win.loadFile("index.html");
@@ -14,10 +17,38 @@ app.whenReady().then(() => {
   createWindow();
 
   app.on("activate", function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
   });
+
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  Menu.setApplicationMenu(mainMenu);
 });
 
 app.on("window-all-closed", function () {
-  if (process.platform !== "darwin") app.quit();
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
 });
+
+//Menu template
+const mainMenuTemplate: any[] = [
+  {
+    label:'File',
+    subMenu: [
+      {
+        label:'Export'
+      },
+      {
+        label:'Quit',
+        click () {
+          app.quit;
+        }
+      }
+    ]
+  },
+  {
+    label: 'Tools'
+  }
+];
