ec56a37d1bf8a33deca7751f3bf468a4ebdbec96
Raffael Elias Schäfer <s20a3c@umwelt-campus.de>
2021-11-07 16:35:25 +0100

    doing some testing

---
+++ scripts/__tests__/color.test.ts
@@ -0,0 +1,11 @@
+import {colorRGB,colorRGBA} from "../modules/draw/util/color";
+
+test("A new colorRGB cant be empty instead it should be black", () => {
+    let c: colorRGB = new colorRGB();
+    expect(c.color).toBe("rgb(0, 0, 0)")
+});
+
+test("A new colorRGBA cant be empty instead it should be black without transparency", () => {
+    let c: colorRGBA = new colorRGBA();
+    expect(c.color).toBe("rgb(0, 0, 0)")
+});
--- scripts/modules/draw/util/color.ts
+++ scripts/modules/draw/util/color.ts
@@ -2,7 +2,7 @@ export class colorRGB {
   r: number;
   g: number;
   b: number;
-  constructor(r: number, g: number, b: number) {
+  constructor(r: number = 0, g: number = 0, b: number = 0) {
     this.r = r;
     this.g = g;
     this.b = b;
@@ -20,7 +20,7 @@ export class colorRGBA {
   g: number;
   b: number;
   a: number;
-  constructor(r: number, g: number, b: number, a: number) {
+  constructor(r: number = 0, g: number = 0, b: number = 0, a: number = 255) {
     this.r = r;
     this.g = g;
     this.b = b;
